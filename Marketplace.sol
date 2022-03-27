3f003b0090296dffc9e79b00009499a97a472c34
DHAVAL PARMAR <53995327+dhavalmp194@users.noreply.github.com>
2022-03-27 23:00:35 +0530

    Modifiers added, platform fees added

--- Marketplace.sol
+++ Marketplace.sol
@@ -2,6 +2,245 @@
 pragma solidity ^0.8.0;
 
 /**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations.
+ *
+ * NOTE: `SafeMath` is generally not needed starting with Solidity 0.8, since the compiler
+ * now has built in overflow checking.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+            // benefit is lost if 'b' is also tested.
+            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
+}
+
+/**
  * @dev Interface of the ERC20 standard as defined in the EIP.
  */
 interface IERC20 {
@@ -256,19 +495,100 @@ interface IERC721 is IERC165 {
     ) external;
 }
 
-contract Marketplace is IERC721Receiver{
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+
+contract Marketplace is IERC721Receiver, Ownable{
+    using SafeMath for uint;
+
+    struct NFTsList{
+        uint256 tokenId;
+        bool isNFTTokenAvailableForSale;
+        address ownerOfListedNFTToken;
+        uint256 priceOfNFTToken;
+        address[] royaltyReceiver;
+    }
     
     IERC721 public nftToken;
     
     IERC20 public token;
     
-    mapping (uint256 => bool) public isNFTTokenAvailableForSale ;
-    
-    mapping (uint256 => bool) public isNFTTokenListedOnMarket ;
-    
-    mapping (uint256 => address) public ownerOfListedNFTToken ;
-    
-    mapping (uint256 => uint256) public priceOfNFTToken ;
+    // Here if fee is 2.5% than set it to 2.5 * 100 = 250
+    uint16 public platformFees = 250;
+
+    uint16 public royaltyFees;
+
+    mapping (uint256 => bool) public isNFTTokenListedOnMarket;
+
+    mapping (uint256 => NFTsList) public nftsList ;
     
     uint256[] public tokensOnSale;
     
@@ -276,58 +596,95 @@ contract Marketplace is IERC721Receiver{
         nftToken = _nftToken;
         token = _token;
     } 
+
+    modifier isTokenListed(uint256 _tokenId){
+        require(isNFTTokenListedOnMarket[_tokenId], "token is not listed");
+        _;
+    }
+
+    modifier isOwnerOfToken(uint256 _tokenId){
+        require(_msgSender() == nftsList[_tokenId].ownerOfListedNFTToken, "not authorized");
+        _;
+    }
+
+    modifier isTokenForSale(uint256 _tokenId){
+        require(!nftsList[_tokenId].isNFTTokenAvailableForSale, "token already on sale");
+        _;
+    }
     
     function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
         return this.onERC721Received.selector;
     }
+
+    function setPlatformFees(uint16 _fee) public onlyOwner returns(bool) {
+        require(_fee <= 10000, "setPlatformFees: max fee can be 100");
+        platformFees = _fee;
+        return true;
+    }
+
+    function setRoyaltyFees(uint16 _fee) public onlyOwner returns(bool) {
+        require(_fee <= 10000, "setPlatformFees: max fee can be 100");
+        royaltyFees = _fee;
+        return true;
+    }
+
+    function updateListedTokenPrice(uint256 _tokenId, uint256 _price) public onlyOwner isTokenListed(_tokenId) isOwnerOfToken(_tokenId) isTokenForSale(_tokenId) returns(bool) {
+        require(_price > 0, "putYourTokenForSale: price can't be zero");
+        require(nftsList[_tokenId].priceOfNFTToken > 0, "putYourTokenForSale: token price should be greater than zero");
+        nftsList[_tokenId].priceOfNFTToken = _price;
+        return true;
+    }
     
-    function listNFTTokenOnMarket(uint256 _tokenId) public {
-        require(msg.sender == nftToken.ownerOf(_tokenId), "listTokenOnMarket: not authorized");
+    function listNFTTokenOnMarket(uint256 _tokenId) public isOwnerOfToken(_tokenId){
         require(!isNFTTokenListedOnMarket[_tokenId], "listTokenOnMarket: token already listed");
         isNFTTokenListedOnMarket[_tokenId] = true;
-        ownerOfListedNFTToken[_tokenId] = msg.sender;
-        nftToken.safeTransferFrom(msg.sender, address(this), _tokenId);
+        nftsList[_tokenId].ownerOfListedNFTToken = _msgSender();
+        nftToken.safeTransferFrom(_msgSender(), address(this), _tokenId);
     }
     
-    function removeNFTTokenFromMarket(uint256 _tokenId) public {
-        require(msg.sender == nftToken.ownerOf(_tokenId), "removeTokenFromMarket: not authorized");
-        require(isNFTTokenListedOnMarket [_tokenId], "removeTokenFromMarket: token is not listed");
+    function removeNFTTokenFromMarket(uint256 _tokenId) public isTokenListed(_tokenId) isOwnerOfToken(_tokenId){
         isNFTTokenListedOnMarket[_tokenId] = false;
-        if(isNFTTokenAvailableForSale[_tokenId]){
-            isNFTTokenAvailableForSale[_tokenId] = false;
+        if(nftsList[_tokenId].isNFTTokenAvailableForSale){
+            nftsList[_tokenId].isNFTTokenAvailableForSale = false;
             removeNFTTokenIdFromSale(_tokenId);
         }
-        ownerOfListedNFTToken [_tokenId] = address(0);
-        nftToken.safeTransferFrom(address(this), msg.sender, _tokenId);
+        nftsList[_tokenId].ownerOfListedNFTToken = address(0);
+        nftToken.safeTransferFrom(address(this), _msgSender(), _tokenId);
     }
     
-    function putYourNFTTokenForSale(uint256 _tokenId, uint256 _price) public {
-        require(msg.sender == ownerOfListedNFTToken [_tokenId], "putYourTokenForSale: not authorized");
-        require(isNFTTokenListedOnMarket[_tokenId], "putYourTokenForSale: token is not listed");
-        require(!isNFTTokenAvailableForSale[_tokenId], "putYourTokenForSale: token already on sale");
-        isNFTTokenAvailableForSale[_tokenId] = true;
-        priceOfNFTToken[_tokenId] = _price;
+    function putYourNFTTokenForSale(uint256 _tokenId, uint256 _price, address[] memory _royaltyReceiver) public isTokenListed(_tokenId) isOwnerOfToken(_tokenId) isTokenForSale(_tokenId){
+        nftsList[_tokenId].isNFTTokenAvailableForSale = true;
+        nftsList[_tokenId].priceOfNFTToken = _price;
+        nftsList[_tokenId].royaltyReceiver = _royaltyReceiver;
         tokensOnSale.push(_tokenId);
     }
     
-    function removeYourNFTTokenFromSale(uint256 _tokenId) public {
-        require(msg.sender == ownerOfListedNFTToken [_tokenId], "removeYourTokenFromSale: not authorized");
-        require(isNFTTokenListedOnMarket[_tokenId], "removeYourTokenFromSale: token is not listed");
-        require(isNFTTokenAvailableForSale[_tokenId], "removeYourTokenFromSale: token is not on sale");
-        isNFTTokenAvailableForSale[_tokenId] = false;
-        priceOfNFTToken[_tokenId] = 0;
+    function removeYourNFTTokenFromSale(uint256 _tokenId) public isTokenListed(_tokenId) isOwnerOfToken(_tokenId){
+        require(nftsList[_tokenId].isNFTTokenAvailableForSale, "removeYourTokenFromSale: token is not on sale");
+        nftsList[_tokenId].isNFTTokenAvailableForSale = false;
+        nftsList[_tokenId].priceOfNFTToken = 0;
         removeNFTTokenIdFromSale(_tokenId);
     }
     
-    function buyNFTFromMarket(uint256 _tokenId, uint256 _amount) public {
-        require(isNFTTokenListedOnMarket[_tokenId], "buyNFTFromMarket: token is not listed");
-        require(isNFTTokenAvailableForSale[_tokenId], "buyNFTFromMarket: token is not on sale");
-        require(_amount == priceOfNFTToken[_tokenId], "buyNFTFromMarket: Insufficient amount");
+    function buyNFTFromMarket(uint256 _tokenId, uint256 _amount) public isTokenListed(_tokenId){
+        require(nftsList[_tokenId].isNFTTokenAvailableForSale, "buyNFTFromMarket: token is not on sale");
+        require(_amount == nftsList[_tokenId].priceOfNFTToken, "buyNFTFromMarket: Insufficient amount");
         isNFTTokenListedOnMarket[_tokenId] = false;
-        isNFTTokenAvailableForSale[_tokenId] = false;
-        token.transferFrom(msg.sender, ownerOfListedNFTToken[_tokenId], _amount);
-        nftToken.safeTransferFrom(address(this), msg.sender, _tokenId);
-        ownerOfListedNFTToken[_tokenId] = address(0);
+        nftsList[_tokenId].isNFTTokenAvailableForSale = false;
+        uint fee = _amount.mul(platformFees).div(1e4);
+        uint royalty = _amount.mul(royaltyFees).div(1e4);
+        token.transferFrom(_msgSender(), address(this), _amount);
+        if(royaltyFees > 0){
+            if(nftsList[_tokenId].royaltyReceiver.length > 0){
+                uint fraction = royaltyFees / nftsList[_tokenId].royaltyReceiver.length;
+                for(uint i = 0; i < nftsList[_tokenId].royaltyReceiver.length; i++){
+                    token.transfer(nftsList[_tokenId].royaltyReceiver[i], fraction);
+                }
+            }
+        }
+        token.transfer(nftsList[_tokenId].ownerOfListedNFTToken, _amount.sub(fee.add(royalty)));
+        nftToken.safeTransferFrom(address(this), _msgSender(), _tokenId);
+        nftsList[_tokenId].ownerOfListedNFTToken = address(0);
         removeNFTTokenIdFromSale(_tokenId);
     }
     
@@ -340,5 +697,9 @@ contract Marketplace is IERC721Receiver{
                tokensOnSale.pop();
             }
         }
+    }
+
+    function transferAnyERC20(IERC20 _tokenAddress, address _beneficiary, uint256 _amount) public {
+        _tokenAddress.transfer(_beneficiary, _amount);
     }
 }
